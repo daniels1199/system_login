267c0d5a79a28743723c0fff53f9f30086de2ebd
Daniel Soares <ds507556@gmail.com>
2026-09-09 05:41:50 -0300

    Corringindo erro de digitação no application.properties

--- src/main/java/com/example/system_login/controller/UsuarioController.java
+++ src/main/java/com/example/system_login/controller/UsuarioController.java
@@ -49,7 +49,7 @@ public class UsuarioController {
         }
         
         Cookie cookie = new Cookie("AUTH_TOKEN", token);
-        cookie.setHttpOnly(true);
+        cookie.setHttpOnly(false);
         cookie.setSecure(true); //Ativa segurança HTTPS, mas desativa o H2 Console
         cookie.setPath("/");
         cookie.setMaxAge(7200);
